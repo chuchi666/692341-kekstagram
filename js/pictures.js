f86e14a60a4d8c213b02c872ed80e430986804c8
chuchi666 <39630615+chuchi666@users.noreply.github.com>
2018-07-04 22:20:02 +0300

    module5-task1

--- js/pictures.js
+++ js/pictures.js
@@ -185,17 +185,14 @@ imageScaleMinus.addEventListener('click', decreaseImageScaleHandler);
 // слайдер глубины
 var scale = document.querySelector('.scale');
 var scalePin = scale.querySelector('.scale__pin');
-var scaleEffectValue = scale.querySelector('.scale__value');
+var scaleLine = document.querySelector('.scale__line');
+var scaleLevel = document.querySelector('.scale__level');
+var lineWidth;
 scale.classList.add('hidden');
 
-var DEFAULT_SCALE_EFFECT_VALUE = 30;
+var DEFAULT_SCALE_EFFECT_VALUE = 1;
 
-scaleEffectValue.value = DEFAULT_SCALE_EFFECT_VALUE;
-scaleEffectValue.setAttribute('min', '0');
-scaleEffectValue.setAttribute('max', '100');
-
-var setFilterValue = function (filterName, filterValue) {
-  var saturation = filterValue / scaleEffectValue.getAttribute('max');
+var setFilterValue = function (filterName, saturation) {
   switch (filterName) {
     case 'effects__preview--chrome':
       uploadPreview.style.filter = 'grayscale(' + saturation + ')';
@@ -225,23 +222,51 @@ var inputEffects = document.querySelectorAll('.effects__radio');
 
 var changeInputEffectHandler = function (evt) {
   var effectId = evt.target.id.split('-')[1];
-  scaleEffectValue.value = DEFAULT_SCALE_EFFECT_VALUE;
   var filterName = 'effects__preview--' + effectId;
   uploadPreview.setAttribute('class', filterName);
   setFilterValue(filterName, DEFAULT_SCALE_EFFECT_VALUE);
   scale.classList.toggle('hidden', effectId === 'none');
+  lineWidth = scaleLine.offsetWidth;
+  scalePin.style.left = lineWidth + 'px';
+  scaleLevel.style.width = lineWidth + 'px';
 };
 
 inputEffects.forEach(function (element) {
   element.addEventListener('click', changeInputEffectHandler);
 });
 
-scalePin.addEventListener('mouseup', function () {
-  scaleEffectValue.value = (Number(scaleEffectValue.value) + 5).toString();
-  setFilterValue(
-      uploadPreview.getAttribute('class'),
-      Number(scaleEffectValue.value)
-  );
+
+// Движение ползунка слайдера
+scalePin.addEventListener('mousedown', function (evt) {
+
+  var startCoords = evt.clientX;
+
+  var moveMouseHandler = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = startCoords - moveEvt.clientX;
+
+    var movePin = scalePin.offsetLeft - shift;
+
+    if (movePin > 0 && movePin < lineWidth) {
+      startCoords = moveEvt.clientX;
+      scalePin.style.left = (scalePin.offsetLeft - shift) + 'px';
+      scaleLevel.style.width = movePin + 'px';
+      setFilterValue(
+          uploadPreview.getAttribute('class'),
+          Number(movePin / lineWidth)
+      );
+    }
+  };
+
+  var upMouseHandler = function (upEvt) {
+    upEvt.preventDefault();
+    document.removeEventListener('mousemove', moveMouseHandler);
+    document.removeEventListener('mouseup', upMouseHandler);
+  };
+
+  document.addEventListener('mousemove', moveMouseHandler);
+  document.addEventListener('mouseup', upMouseHandler);
 });
 
 // закрытие большого фото
